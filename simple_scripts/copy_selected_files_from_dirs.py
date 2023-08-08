444b04b84698bf7ab9d5886351a950fb9c17f3f9
Gerald Liu <36258909+gerald-liu@users.noreply.github.com>
2023-08-08 15:40:38 +0800

    Add files via upload

--- simple_scripts/copy_selected_files_from_dirs.py
+++ simple_scripts/copy_selected_files_from_dirs.py
@@ -3,17 +3,19 @@ import shutil
 import sys
 
 home_path = 'F:\\LWY\\OneDrive - HKUST Connect\\Abama\\新股'
-target_path = "F:\\Upload\\新股研究"
+target_path = "F:\\Upload\\新股报告"
 author_name = sys.argv[1]
+start_date = sys.argv[2]
 
-def get_target_basename(filename, ext, author):
-    return filename + '_' + author + ext
+def get_target_basename(filename: str, ext: str, author: str) -> str:
+    return f'{filename}_{author}{ext}'
 
-def is_file1(filename, ext):
+def is_file1(filename: str, ext: str) -> bool:
     return ext == '.pdf' and '研究报告' in filename and len(filename) <= 8
 
-def is_file2(filename):
+def is_file2(filename: str) -> bool:
     return '初稿' in filename or '新股介绍' in filename
+
 
 for home_item in os.scandir(home_path):
     
@@ -21,7 +23,14 @@ for home_item in os.scandir(home_path):
         folder_path = home_item.path
         folder_name = os.path.basename(folder_path)
         
-        target_folder_path = os.path.join(target_path, folder_name)
+        folder_date = folder_name[0:8]
+        if folder_date < start_date:
+            continue
+        
+        folder_args = folder_name.split('_')
+        out_folder_name = folder_args[0]
+        
+        target_folder_path = os.path.join(target_path, out_folder_name)
         
         for folder_item in os.scandir(folder_path):
             if folder_item.is_file():
